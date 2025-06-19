cb5935af72d2b1bd8618b3466ea3f4e982152545
Samuel David Roncal Vidal <davidroncal@MacBook-Pro-de-Samuel-2.local>
2025-06-19 14:11:20 -0500

    rebase

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,7 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig({
+  base: '/control_asistencia_react_upeu/', // Muy importante
   plugins: [react()],
   server: {
     port: 5173
